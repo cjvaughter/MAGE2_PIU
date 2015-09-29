c9359392748e5f7df579df67ecf65baaf9a56294
Jacob Dixon <jacob.dixon@okstate.edu>
2015-09-29 16:15:49 -0500

    (semi)-ported XBee library to C++
    Its kinda there. Not Tested!!

--- MAGE2_PIU/Library/XBee/XBee.cpp
+++ MAGE2_PIU/Library/XBee/XBee.cpp
@@ -1,7 +1,191 @@
 /*******************************************************************************
 * Name: XBee.cpp
-* Description: 
-* Author: CJ Vaughter
+* Description: XBEE control library for use with the MAGE 2 PIU
+* Author: CJ Vaughter & Jacob Dixon
 * Created: 9/21/2015 2:26:59 PM
+* Updated: 9/29/2015 Rev. A - Jacob Dixon
 *******************************************************************************/
 
+#include "XBee.h"
+
+XBeeClass XBee;
+
+void XBeeClass::CurrentMessage()
+{
+	//From C#:
+	/*
+	get
+            {
+                MAGEMsg temp = _currentMessage;
+                _currentMessage = null;
+                Ready = false;
+                return temp;
+            }
+            private set
+            {
+                _currentMessage = value;
+                Ready = true;
+            }
+	*/
+}
+
+void XBeeClass::Decode(uint8_t data)
+{
+	if (data == Escape)
+    {
+        _escape = true;
+        return;
+    }
+    if (_escape)
+    {
+        data ^= XOR;
+        _escape = false;
+    }
+	switch (_step)
+	{
+		case 0:
+			if (data == Delimiter) _step++;
+			break;
+		case 1:
+			_length = (uint8_t)(data << 8);
+			_step++;
+			break;
+		case 2:
+			_length |= data;
+			_step++;
+			break;
+		case 3:
+			_api = data;
+			_sum = data;
+			if (_api != RX) _step = 0;
+			else _step++;
+			break;
+		case 4:
+			_address = ((long)data) << 56;
+			_sum += data;
+			_step++;
+			break;
+		case 5:
+			_address |= ((long)data) << 48;
+			_sum += data;
+			_step++;
+			break;
+		case 6:
+			_address |= ((long)data) << 40;
+			_sum += data;
+			_step++;
+			break;
+		case 7:
+			_address |= ((long)data) << 32;
+			_sum += data;
+			_step++;
+			break;
+		case 8:
+			_address |= ((long)data) << 24;
+			_sum += data;
+			_step++;
+			break;
+		case 9:
+			_address |= ((long)data) << 16;
+			_sum += data;
+			_step++;
+			break;
+		case 10:
+			_address |= ((long)data) << 8;
+			_sum += data;
+			_step++;
+			break;
+		case 11:
+			_address |= ((long)data);
+			_sum += data;
+			_step++;
+			break;
+		case 12:
+			_address16 = (uint8_t)(data << 8);
+			_sum += data;
+			_step++;
+			break;
+		case 13:
+			_address16 |= data;
+			_sum += data;
+			_step++;
+			break;
+		case 14: //Ignore options
+			_sum += data;
+			_step++;
+			break;
+		case 15:
+			_data = new byte[_length - RX_Header];
+			_index = 0;
+			_data[_index++] = data;
+			_sum += data;
+			_step++;
+			break;
+		default:
+			_data[_index++] = data;
+			_sum += data;
+			if (_index == _length - RX_Header) _step = 255;
+			break;
+		case 255:
+			_checksum = data;
+			if (Check - _sum == _checksum)
+			{
+				CurrentMessage = new MAGEMsg(_address, _data);
+			}
+			_step = 0;
+			break;
+    }
+}
+
+void XBeeClass::Encode(uint8_t[] data)
+{
+	//From C#:
+	/*
+	byte sum = 0;
+	List<byte> data = new List<byte>();
+	data.Add(Delimiter);
+	data.Add((byte)((TX_Header + msg.Data.Length) >> 8));
+	data.Add((byte)(TX_Header + msg.Data.Length));
+	data.Add(TX);
+	data.Add(0); //No response
+	data.Add((byte)(msg.Address >> 56));
+	data.Add((byte)(msg.Address >> 48));
+	data.Add((byte)(msg.Address >> 40));
+	data.Add((byte)(msg.Address >> 32));
+	data.Add((byte)(msg.Address >> 24));
+	data.Add((byte)(msg.Address >> 16));
+	data.Add((byte)(msg.Address >> 8));
+	data.Add((byte)(msg.Address));
+	data.Add(DefaultAddress16High);
+	data.Add(DefaultAddress16Low);
+	data.Add(0); //Maximum network hops
+	data.Add(0); //No options
+
+	foreach (byte b in msg.Data)
+	{
+		data.Add(b);
+	}
+
+	for (int i = 3; i < data.Count; i++) sum += data[i];
+	data.Add((byte)(Check - sum));
+
+	for (int i = 3; i < data.Count - 1; i++)
+	{
+		if (data[i] == Delimiter || data[i] == Escape || data[i] == XON || data[i] == XOFF)
+		{
+			data[i] ^= XOR;
+			data.Insert(i, Escape);
+		}
+	}
+
+	return data.ToArray();
+	*/
+}
+
+void XBeeClass::MAGEMsg(long address, uint8_t[] data)
+{
+	Address = address;
+	Data = data;
+}
+
+ 
--- MAGE2_PIU/Library/XBee/XBee.h
+++ MAGE2_PIU/Library/XBee/XBee.h
@@ -1,16 +1,77 @@
 /*******************************************************************************
 * Name: XBee.h
-* Description: 
-* Author: CJ Vaughter
+* Description: Header file for Digi's XBEE DigiMesh for use on the PIU
+* Author: CJ Vaughter & Jacob Dixon
 * Created: 9/21/2015 2:26:27 PM
+* Updated: 9/29/2015 Rev. A - Jacob Dixon
 *******************************************************************************/
  
 #ifndef XBEE_h
 #define XBEE_h
 
-class XBee
+#include "Arduino.h"
+
+//XBEE API frame definitions
+#define Delimiter = 0x7e;
+#define TX = 0x10;
+#define TX_Header 
+#define RX = 0x90;
+#define RX_Header = 0x0C;
+#define Escape = 0x7D;
+#define XON = 0x11;
+#define XOFF = 0x13;
+#define XOR = 0x20;
+#define Check = 0xFF;
+#define DefaultAddress16High = 0xFF;
+#define DefaultAddress16Low = 0xFE;
+
+public struct IRID
 {
-	
+    public uint8_t ID;
+    public uint8_t Func;
+    public uint8_t Uni;
+    public long Timestamp;
 };
+
+
+public enum MsgFunc
+{
+    Heartbeat,
+    Connect,
+    SentSpell,
+    RecievedSpell,
+};
+ 
+class XBeeClass
+{
+	public:
+		void CurrentMessage();
+		void Decode(uint8_t data);
+		void Encode(uint8_t[] data);
+		void MAGEMsg(long address, uint8_t[] data);
+		static bool getReady;
+		static bool setReady;
+		long Address;
+		uint8_t[] Data;
+		public MsgFunc Func;
+		public uint8_t ID;
+		
+	private:
+		String _currentMessage;
+		static int _step;
+		static uint8_t _api;
+		static uint8_t _length;
+		static long _address;
+		static int _address16;
+		static uint8_t[] _data;
+		static uint8_t _checksum;
+		static bool _escape;
+		static uint8_t _sum;
+		static uint8_t _index;
+		
+};
+
+extern XBeeClass XBee;
+
 
 #endif
